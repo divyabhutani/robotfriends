3f6088264836ce6e2deebe6d37db3d13d2b12cec
divya <divyabhutani33@gmail.com>
2020-09-22 22:11:04 +0530

    Updates

---
+++ precache-manifest.353a246dae98f684206081d25fbf4b5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf244039971dffdc089c54dd064c7e3",
+    "url": "//rebotfriends/index.html"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "//rebotfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d18c85e5b57d6a1afa0e",
+    "url": "//rebotfriends/static/css/main.c7b51085.chunk.css"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "//rebotfriends/static/js/2.f62c00ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//rebotfriends/static/js/2.f62c00ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18c85e5b57d6a1afa0e",
+    "url": "//rebotfriends/static/js/main.10c4c5a5.chunk.js"
+  },
+  {
+    "revision": "4d485f8bc39f62dad893",
+    "url": "//rebotfriends/static/js/runtime-main.ed276557.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "//rebotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
